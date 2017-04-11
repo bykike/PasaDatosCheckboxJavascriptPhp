172db82d9937eaa03cfb8a6993fbc4d2e7353494
Kike <kike@areavisual.net>
2017-04-11 17:52:26 +0200

    ChackboxdeJavascriptaPhp
    Paso datos de una lista de checkbox por medio de Javascript a Php.

---
+++ VerCheck.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+
+    <title>Recibe datos</title>
+    
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/docs.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/miCSS.css">
+
+    <meta charset="UTF-8">
+       
+</head>
+
+   <div class="container">   
+    <div class="row">
+
+
+
+<?php
+
+
+
+        /* Sección Datos Personales */
+
+        $VariableCheck  = "<script> document.write(VariableCheck) </script>";
+        echo $VariableCheck;
+
+
+?>
+        <div class="col-md-12">
+            <h1>Proceso de registro</h1>
+        </div>
+
+        <hr>
+        <br><br>
+
+        <div align="center">
+            Su formulario se ha enviado con éxito. Nos pondremos en contacto con usted en el momento lo requiramos. Gracias. <br/> </div>
+        </div>
+
+        <hr>
+        <br><br>
+
+        <div align="center">
+            <!-- Volver apantalla inicial -->
+            <input class="btn btn-default" type="button" value="Volver a la web" onclick="location.href='-index.html'">
+            <br><br>
+        </div>
+   
+        </div>
+      </div>  
+
+    </body>
+</html>
